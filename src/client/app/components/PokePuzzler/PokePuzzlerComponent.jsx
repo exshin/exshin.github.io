7b0e133093dc3dd5953e3488d1eed2042b1eb893
Eugene Chinveeraphan <chinveeraphan@gmail.com>
2017-07-08 01:18:57 -0700

    Updated pokepuzzler pics to use cards

--- src/client/app/components/PokePuzzler/PokePuzzlerComponent.jsx
+++ src/client/app/components/PokePuzzler/PokePuzzlerComponent.jsx
@@ -13,29 +13,59 @@ class PokePuzzlerComponent extends React.Component {
 
     return (
       <div className="container-fluid">
-        <div className="pokepuzzler-img float-left">
-          Select your Pokemon. This screen is a placeholder for when I finish the "Your Pokemon" Collections view.
-          <img className="rounded img-fluid pp-img" src="./src/client/img/pp_select.png"></img>
+
+        <div className="row padding-bottom-30">
+          <div className="col-4 pp-col">
+            <div className="card pp-card">
+              <img className="card-img-top pp-img" src="./src/client/img/pp_select.PNG"></img>
+              <div className="card-block pp-text-block">
+                <p className="card-text pp-text">Select your Pokemon. This screen is a placeholder for when I finish the "Your Pokemon" Collections view.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 pp-col">
+            <div className="card pp-card">
+              <img className="card-img-top pp-img" src="./src/client/img/pp_battle_start.PNG"></img>
+              <div className="card-block pp-text-block">
+                <p className="card-text pp-text">Battle Start! We start the battle with random energy tiles based on your Pokemon's 3 skill moves.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 pp-col">
+            <div className="card pp-card">
+              <img className="card-img-top pp-img" src="./src/client/img/pp_moved.PNG"></img>
+              <div className="card-block pp-text-block">
+                <p className="card-text pp-text">Made a match. Charmander gets 3 moves to make matches per turn, based on your speed and evolution tier.</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="pokepuzzler-img float-left">
-          Battle Start! We start the battle with random energy tiles based on your Pokemon's 3 skill moves.
-          <img className="rounded img-fluid pp-img" src="./src/client/img/pp_battle_start.png"></img>
-        </div>
-        <div className="pokepuzzler-img float-left">
-          Made a match. Charmander gets 3 moves to make matches per turn (dependent on your speed and evolution tier).
-          <img className="rounded img-fluid pp-img" src="./src/client/img/pp_moved.png"></img>
-        </div>
-        <div className="pokepuzzler-img float-left">
-          During the opponent's turn, they collect energy and use their moves if they have enough energy to. Then it is our turn again!
-          <img className="rounded img-fluid pp-img" src="./src/client/img/pp_your_turn.png"></img>
-        </div>
-        <div className="pokepuzzler-img float-left">
-          Used Ember. We collected enough of the fire energy on the board to use this move. It does damage AND destroys random tiles on the board.
-          <img className="rounded img-fluid pp-img" src="./src/client/img/pp_ember.png"></img>
-        </div>
-        <div className="pokepuzzler-img float-left">
-          We won the fight! We collect in-game currency and experience which will level our Pokemon. This screen will also get a facelift later.
-          <img className="rounded img-fluid pp-img" src="./src/client/img/pp_battle_won.png"></img>
+
+        <div className="row">
+          <div className="col-4 pp-col">
+            <div className="card pp-card">
+              <img className="card-img-top pp-img" src="./src/client/img/pp_your_turn.PNG"></img>
+              <div className="card-block pp-text-block">
+                <p className="card-text pp-text">During the opponent's turn, they collect energy and use their moves if they have enough energy to. Then it is our turn again!</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 pp-col">
+            <div className="card pp-card">
+              <img className="card-img-top pp-img" src="./src/client/img/pp_ember.PNG"></img>
+              <div className="card-block pp-text-block">
+                <p className="card-text pp-text">Used Ember. We collected enough of the fire energy on the board to use this move. It does damage AND destroys random tiles on the board.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 pp-col">
+            <div className="card pp-card">
+              <img className="card-img-top pp-img" src="./src/client/img/pp_battle_won.PNG"></img>
+              <div className="card-block pp-text-block">
+                <p className="card-text pp-text">We won the fight! We collect in-game currency and experience which will level our Pokemon. This screen will also get a facelift later.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )

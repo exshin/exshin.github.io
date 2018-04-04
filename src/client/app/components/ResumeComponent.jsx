a294869585d5a800639cf62894935e77a652db67
Eugene Chinveeraphan <chinveeraphan@gmail.com>
2018-04-03 23:26:23 -0700

    Fix

--- src/client/app/components/ResumeComponent.jsx
+++ src/client/app/components/ResumeComponent.jsx
@@ -8,7 +8,7 @@ class ResumeComponent extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { workerLevel: "Developer" }
+    this.state = { workerLevel: "ZenDeveloper" }
     this.toWorker = this.toWorker.bind(this)
   }
 
